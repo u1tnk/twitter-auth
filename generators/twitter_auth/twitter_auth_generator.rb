09d20a3fe6f380d3615780ebdbf13398972e1780
Jonathan Conway <jonathan@jaikoo.com>
2010-02-20 16:17:05 +0000

    working on updating the generator

--- generators/twitter_auth/twitter_auth_generator.rb
+++ generators/twitter_auth/twitter_auth_generator.rb
@@ -1,34 +1,27 @@
-class TwitterAuthGenerator < Rails::Generator::Base
-  default_options :oauth => true, :basic => false
+require 'rails/generators/named_base'
 
-  def manifest
-    record do |m|
-      m.class_collisions 'User'
-
-      m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => 'twitter_auth_migration'
-      m.template 'user.rb', File.join('app','models','user.rb')
-      m.template 'twitter_auth.yml', File.join('config','twitter_auth.yml')
-    end
-  end
-
-  protected
-
-  def banner
-    "Usage: #{$0} twitter_auth"
-  end
-
-  def add_options!(opt)
-    opt.separator ''
-    opt.separator 'Options:'
+module TwitterAuth
+  module Generators
+    class TwitterAuthGenerator < Rails::Generators::Base
+      argument :authtype, :type => :string, :default => "oauth", :banner => "auth=authtype (oauth/basic)"
+      
+      
+      def self.source_root
+        @_twitter_auth_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+      end
+      
+      desc "Setup twitter auth"
+      def create_twitter_auth_setup
+        check_class_collision 'User'
+        if options[:authtype] == 'basic'
+          options[:basic] = true
+        else
+          options[:oauth] = true
+        end
+        migration_template 'migration.rb', 'db/migrate', :migration_file_name => 'twitter_auth_migration'
+        template 'user.rb', File.join('app','models','user.rb')
+        template 'twitter_auth.yml', File.join('config','twitter_auth.yml')
+      end
     
-    opt.on('-O', '--oauth', 'Use the OAuth authentication strategy to connect to Twitter. (default)') { |v| 
-      options[:oauth] = v
-      options[:basic] = !v
-    }
-
-    opt.on('-B', '--basic', 'Use the HTTP Basic authentication strategy to connect to Twitter.') { |v| 
-      options[:basic] = v
-      options[:oauth] = !v
-    }
   end
 end
